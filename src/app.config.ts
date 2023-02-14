d3ab7952daab16f22d7e24470515f41bedad4345
hbcraft <admin@hbcraft.cn>
2023-02-14 16:10:49 +0800

    feat(main): :zap: 添加全局api前缀,添加配置文件

---
+++ src/app.config.ts
@@ -0,0 +1,12 @@
+/**
+ * api主版本号
+ */
+export const MAIN_VERSION = 1;
+/**
+ * api全局前缀
+ */
+export const GLOBAL_PREFIX = `/v${MAIN_VERSION}/api`;
+/**
+ * 服务器监听端口
+ */
+export const LISTEN_PORT = 3000;
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
+import { GLOBAL_PREFIX, LISTEN_PORT } from './app.config';
 import { AppModule } from './app.module';
 
+/**
+ * 启动应用
+ */
 async function bootstrap() {
+  // 创建应用实例
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  // 设置全局api前缀
+  app.setGlobalPrefix(GLOBAL_PREFIX);
+  // 监听端口
+  await app.listen(LISTEN_PORT);
 }
 bootstrap();
